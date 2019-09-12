8d424ebca5714cdc9bf80ffed57dcb040287e458
云博 <yunbo@weierai.com>
2019-09-12 14:18:17 +0800

    feat:点击this.后面的方法跳转到request页面

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+// const vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,24 +12,15 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jump-vscode-plugin" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	console.log('恭喜, your extension "jump-vscode-plugin" is now active!');
+	require('./src/jump.js')(context);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	console.log('您的插件已被释放！')
+}
 
 module.exports = {
 	activate,
---
+++ src/jump.js
@@ -0,0 +1,33 @@
+const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
+
+function provideDefinition (document, position) {
+  const fileName = document.fileName;
+  const workDir = path.dirname(fileName);
+  const destPath = `${workDir.split('src')[0]}src/api/request.js`;
+  const word = document.getText(document.getWordRangeAtPosition(position));
+  const line = document.lineAt(position).text
+  if(line.trim().startsWith(`this.$api.${word}`)) {
+    if (destPath) {
+      let index = 0;
+      const file = fs.readFileSync(destPath);
+      const lineCode = file.toString('utf-8').split('\n');
+      for (let i = 0; i < lineCode.length; i ++) {
+        let code = lineCode[i];
+        if (code.trim().startsWith(`export const ${word}`)) {
+          index = i
+          break;
+        }
+      }
+      return new vscode.Location(vscode.Uri.file(destPath), new vscode.Position(index, 12));
+    }
+  }
+}
+
+module.exports = function (context) {
+  // 注册如何实现跳转到定义，第一个参数表示仅对javascript文件生效
+  context.subscriptions.push(vscode.languages.registerDefinitionProvider(['vue'], {
+    provideDefinition
+  }));
+};
